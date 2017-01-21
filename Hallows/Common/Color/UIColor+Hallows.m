c7d9ae942df43207586045b5f9a86067b5231a05
Mingle Chang <mingle890805@gmail.com>
2017-01-21 21:26:57 +0800

    not

--- Hallows/Common/Color/UIColor+Hallows.m
+++ Hallows/Common/Color/UIColor+Hallows.m
@@ -11,7 +11,7 @@
 @implementation UIColor (Hallows)
 
 + (UIColor *)zh_navigationColor {
-    return [UIColor darkGrayColor];
+    return [UIColor colorWithRed:202 / 255.0 green:49 / 255.0 blue:37 / 255.0 alpha:1.0];
 }
 
 + (UIColor *)zh_navigationItemColor {
